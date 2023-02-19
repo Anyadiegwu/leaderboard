cb1d76133976e7be73f16a171b461ae8eb897f77
Anyadiegwu <victoranyadiegwu610@gmail.com>
2023-02-19 18:35:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,243 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Leanrnable Leaderboard</h1>
+      </header>
+      <main>
+        <table>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Task 1</th>
+            <th>Task 2</th>
+            <th>Task 3</th>
+            <th>Task 4</th>
+            <th>Task 5</th>
+            <th>Attendance%</th>
+          </tr>
+          <tr>
+            <td>Joshua</td>
+            <td>Philips</td>
+            <td>9</td>
+            <td>10</td>
+            <td>10</td>
+            <td>9</td>
+            <td>10</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>Prince</td>
+            <td>Nnamdi</td>
+            <td>9</td>
+            <td>8</td>
+            <td>10</td>
+            <td>10</td>
+            <td>10</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>Joy</td>
+            <td>James</td>
+            <td>10</td>
+            <td>6</td>
+            <td>10</td>
+            <td>9</td>
+            <td>10</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>Grace</td>
+            <td>Damian</td>
+            <td>10</td>
+            <td>5</td>
+            <td>8</td>
+            <td>8</td>
+            <td>10</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>Victor</td>
+            <td>Henry</td>
+            <td>9</td>
+            <td>10</td>
+            <td>10</td>
+            <td>2</td>
+            <td>6</td>
+            <td>90</td>
+          </tr>
+          <tr>
+            <td>Jessica</td>
+            <td>John</td>
+            <td>10</td>
+            <td>5</td>
+            <td>5</td>
+            <td>5</td>
+            <td>9</td>
+            <td>90</td>
+          </tr>
+          <tr>
+            <td>Charles</td>
+            <td>James</td>
+            <td>9</td>
+            <td>8</td>
+            <td>6</td>
+            <td>4</td>
+            <td>4</td>
+            <td>85</td>
+          </tr>
+          <tr>
+            <td>Charity</td>
+            <td>Philips</td>
+            <td>10</td>
+            <td>6</td>
+            <td>4</td>
+            <td>4</td>
+            <td>2</td>
+            <td>80</td>
+          </tr>
+          <tr>
+            <td>Precious</td>
+            <td>Thompson</td>
+            <td>6</td>
+            <td>6</td>
+            <td>5</td>
+            <td>8</td>
+            <td>4</td>
+            <td>75</td>
+          </tr>
+          <tr>
+            <td>Raphael</td>
+            <td>Johnson</td>
+            <td>6</td>
+            <td>5</td>
+            <td>7</td>
+            <td>4</td>
+            <td>4</td>
+            <td>75</td>
+          </tr>
+        </table>
+
+
+        <table>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Task 1</th>
+            <th>Task 2</th>
+            <th>Task 3</th>
+            <th>Task 4</th>
+            <th>Task 5</th>
+            <th>Attendance%</th>
+          </tr>
+          <tr>
+            <td>Joseph</td>
+            <td>Okeke</td>
+            <td>10</td>
+            <td>10</td>
+            <td>10</td>
+            <td>8</td>
+            <td>9</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>Joy</td>
+            <td>Prince</td>
+            <td>8</td>
+            <td>10</td>
+            <td>9</td>
+            <td>9</td>
+            <td>10</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>Chioma</td>
+            <td>Uche</td>
+            <td>9</td>
+            <td>6</td>
+            <td>10</td>
+            <td>9</td>
+            <td>10</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>Grace</td>
+            <td>Damian</td>
+            <td>9</td>
+            <td>5</td>
+            <td>8</td>
+            <td>8</td>
+            <td>10</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>Victor</td>
+            <td>Henry</td>
+            <td>9</td>
+            <td>10</td>
+            <td>8</td>
+            <td>4</td>
+            <td>6</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>Gift</td>
+            <td>John</td>
+            <td>8</td>
+            <td>5</td>
+            <td>5</td>
+            <td>5</td>
+            <td>9</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>Charles</td>
+            <td>Chike</td>
+            <td>10</td>
+            <td>6</td>
+            <td>6</td>
+            <td>4</td>
+            <td>3</td>
+            <td>90</td>
+          </tr>
+          <tr>
+            <td>Blessing</td>
+            <td>Chima</td>
+            <td>10</td>
+            <td>5</td>
+            <td>4</td>
+            <td>4</td>
+            <td>2</td>
+            <td>80</td>
+          </tr>
+          <tr>
+            <td>Hope</td>
+            <td>Victor</td>
+            <td>6</td>
+            <td>3</td>
+            <td>7</td>
+            <td>8</td>
+            <td>3</td>
+            <td>75</td>
+          </tr>
+          <tr>
+            <td>Chima</td>
+            <td>John</td>
+            <td>5</td>
+            <td>5</td>
+            <td>7</td>
+            <td>3</td>
+            <td>4</td>
+            <td>75</td>
+          </tr>
+        </table>
+      </main>
+
+    </>
+  );
+}
+
+export default App;
